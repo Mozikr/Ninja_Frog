9910968c90cfbcad53320367c6890aa1dee7022e
Szymon Mozol <52745180+Mozikr@users.noreply.github.com>
2024-01-31 18:41:21 +0100

    Create Enemy_Sideways.cs

---
+++ Scripts/Enemy_Sideways.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Enemy_Sideways : MonoBehaviour
+{
+    [SerializeField] private float movementDistance;
+    [SerializeField] private float speed;
+    private bool _movingLeft;
+    private float _leftEdge;
+    private float _rightEdge;
+
+    private void Awake()
+    {
+        _leftEdge = transform.position.x - movementDistance;
+        _rightEdge = transform.position.x + movementDistance;
+    }
+
+    void Update()
+    {
+        if (_movingLeft)
+        {
+            if(transform.position.x > _leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                _movingLeft = false;
+                transform.Rotate(0.0f, 180f, 0.0f);
+            }
+        }
+        else
+        {
+            if (transform.position.x < _rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                _movingLeft = true;
+                transform.Rotate(0.0f, -180f, 0.0f);
+            }
+        }
+    }
+    }
